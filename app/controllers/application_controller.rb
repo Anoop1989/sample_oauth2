d810d10c19ec5013eb1b9addf1e90908e4603c64
Anoop R <ranoop@reflektive.com>
2019-09-23 18:30:20 +0530

    User model and doorkeeper setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+
+  def current_user
+    User.find_by(email: 'pailwan@gmail.com')
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if User.find_by(email: 'pailwan@gmail.com').blank?
+  User.create(name: 'pailwan', email: 'pailwan@gmail.com')
+end
+
