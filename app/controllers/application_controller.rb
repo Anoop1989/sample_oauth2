ae925aa75de7d9caef03456a28a78c1d31669923
Anoop R <ranoop@reflektive.com>
2019-09-24 07:19:55 +0530

    Customise error response and enabled refresh token flow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 
+  def doorkeeper_unauthorized_render_options(error: nil)
+    { json: { error: "Not authorized" } }
+  end
+
   def current_user
     User.find_by(email: 'pailwan@gmail.com')
   end
